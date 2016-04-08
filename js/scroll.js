0aa86df1ec3cab03da49bd652bcb25d0fab7fdfc
Crazier-qi <331538891@qq.com>
2016-04-08 23:53:40 +0800

    js文件

---
+++ js/scroll.js
@@ -0,0 +1,31 @@
+function scroll(wraper,prev,next,i,speed,or)
+ {
+  var wraper = $(wraper);
+  var prev = $(prev);
+  var next = $(next);
+  var i = $(i).find('ul');
+  var w = i.find('li').outerWidth(true);
+  var s = speed;
+  next.click(function()
+       {
+        i.animate({'margin-left':-w},function()
+                  {
+                   i.find('li').eq(0).appendTo(i);
+                   i.css({'margin-left':0});
+                   });
+        });
+  prev.click(function()
+       {
+        i.find('li:last').prependTo(i);
+        i.css({'margin-left':-w});
+        i.animate({'margin-left':0});
+        });
+  if (or == true)
+  {
+   ad = setInterval(function() { next.click();},s*1000);
+   wraper.hover(function(){clearInterval(ad);},function(){ad = setInterval(function() { next.click();},s*1000);});
+
+  }
+ }
+ scroll('.img-scroll','.prev','.next','.img-list',3,true);
+ 
